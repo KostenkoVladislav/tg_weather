ad13909314a9fbc9bfb2eff8d3052ab855705e09
Костенко Владислав Игоревич <kvladig@ya.ru>
2023-10-15 23:32:18 +0300

    1.0
    worked version
    
    view weather for today and all
    
    many changes

--- main.py
+++ main.py
@@ -2,66 +2,18 @@ import telebot
 from telebot import types
 from loguru import logger
 import time
-import text_response
 import weather_api
 from config import *
 
+
+
 bot = telebot.TeleBot(mconf('tg'))
-
-
-def today_send(call):
-    bot.send_message(call.from_user.id, text_response.today_response_text)
-    return
-
-
-def tomorrow_send(call):
-    bot.send_message(call.from_user.id, 'завтра')
-    return
-
-def week_send(call):
-    bot.send_message(call.from_user.id, 'неделя')
-    return
-
-def week2_send(call):
-    bot.send_message(call.from_user.id, 'две недели')
-    return
-
-def moon_send(call):
-    bot.send_message(call.from_user.id, 'луна')
-    return
-
-
-def response_weather(message):
-    bot.send_message(message.from_user.id,'')
-
-
-
-
-'''
-                     f'<b>Доброго утра и прекрасного настроения\n\n</b>'
-                             f'Погода сегодня в славном городе <b>Краснодаре</b> \nпо данным сервиса open-meteo.\n\n'
-                             f'{current_time}\n'
-                             f'<b>00:00   </b>{weather_today["weather_0"]} °C\n'
-                             f'<b>06:00   </b>{weather_today["weather_6"]} °C\n'
-                             f'<b>09:00   </b>{weather_today["weather_9"]} °C\n'
-                             f'<b>12:00   </b>{weather_today["weather_12"]} °C\n'
-                             f'<b>15:00   </b>{weather_today["weather_15"]} °C\n'
-                             f'<b>18:00   </b>{weather_today["weather_18"]} °C\n'
-                             f'<b>21:00   </b>{weather_today["weather_21"]} °C\n',
-                     parse_mode="html")
-'''
-
-
-# print(data, res)
-
-def adm(iid):
-    bot.send_message(759333833, f'Hey, new \'/start\' user.\n Id= @{iid}')
 
 
 @bot.message_handler(commands=['start'])
 # start и инициализация кнопок под сообщением
 def start_message(message):
-    adm(message.chat.username)
+    logger.info(f'{message.chat.username}, command = start')
     keyboard = types.InlineKeyboardMarkup()
     key_today = types.InlineKeyboardButton(text='Сегодня', callback_data='today')
     keyboard.add(key_today)
@@ -77,15 +29,15 @@ def start_message(message):
 
 @bot.callback_query_handler(func=lambda call: True)  # присвоение группы
 def callback_worker(call):
-    call_dict = {'today': today_send,
-                 'tomorrow': tomorrow_send,
-                 'week': week_send,
-                 'week2': week2_send,
-                 'moon': moon_send
+    call_dict = {'today': [weather_api.weather_get('today'), 'сегодня'],
+                 'tomorrow': [weather_api.weather_get('tomorrow'), 'завтра'],
+                 'week': [weather_api.weather_get('week'), 'неделю'],
+                 'week2': [weather_api.weather_get('week2'), 'две недели']
                  }
 
-    call_dict[call.data](call)
+    logger.info(f'{call.data}, {call.from_user.username}')
 
+    bot.send_message(call.from_user.id, f'{call_dict[call.data][0]}', parse_mode="html")
 
 @bot.message_handler(commands=['weather'])
 def weather_message(message):
@@ -97,7 +49,8 @@ def get_text_messages(message):
     bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /start.")
 
 
+import time
 import asyncio
-# time.sleep(5)
+time.sleep(2)
 logger.info(f"Application successful running")
 asyncio.run(bot.polling())
--- weather_api.py
+++ weather_api.py
@@ -1,23 +1,100 @@
-import time
+from time import gmtime, strftime
 import threading
 import requests
 from loguru import logger
 import config
 import os
-import inspect
-global json_wt_req
-from time import gmtime, strftime
-import text_response
+import time
 
-timeout = 60*60  # Тайм-аут запроса погоды(сек)
+global json_wt_req
+
+
+timeout = 60 * 60  # Тайм-аут запроса погоды(сек)
 current_file = os.path.basename(__file__)
 logger.add(config.logger_path, format='{time} <{level}> {message}',
            rotation="10MB", compression="zip")
 
+def wind_direction_to_word(count):
+    count = int(count)
+# На вход подаётся число от 0 до 360 (градусы), направление ветра
+# На выход ожидается условное буквенное обозначение
+    if count < 22:
+        return 'Северо-Восток'
+    elif count < 67:
+        return 'Восток'
+    elif count < 112:
+        return 'Юго-Восток'
+    elif count < 157:
+        return 'Юг'
+    elif count < 202:
+        return 'Юго-Запад'
+    elif count < 247:
+        return 'Запад'
+    elif count < 292:
+        return 'Северо-Запад'
+    elif count < 337:
+        return 'Север'
+    else:
+        return 'Север'
+
+
+
+
+def weather_response_text(json, last_update_time):
+    global today_response_text
+    global tomorrow_response_text
+    global week_response_text
+    global week2_response_text
+    header = str(f'<b>Доброго утра и прекрасного настроения\n\n</b>'
+                 f'Погода сегодня в славном городе <b>Краснодаре</b> \nпо данным сервиса open-meteo:\n\n'
+                 f'Время МСК\n{last_update_time}\n\n')
+
+    today_response_text = header + (
+        f'00:00\n'
+        f'    Температура: <b>{json["hourly"]["temperature_2m"][0]}</b>{json["hourly_units"]["temperature_2m"]}\n'
+        f'    Ветер:                <b>{json["hourly"]["windspeed_10m"][0]}</b> {json["hourly_units"]["windspeed_10m"]}'
+        f'\n    Направление: {wind_direction_to_word(json["hourly"]["winddirection_10m"][0])}\n'
+
+
+        f'06:00\n'
+        f'    Температура: <b>{json["hourly"]["temperature_2m"][6]}</b>{json["hourly_units"]["temperature_2m"]}\n'
+        f'    Ветер:                <b>{json["hourly"]["windspeed_10m"][6]}</b> {json["hourly_units"]["windspeed_10m"]}'
+        f'\n    Направление: {wind_direction_to_word(json["hourly"]["winddirection_10m"][6])}\n'
+
+        f'09:00\n'
+        f'    Температура: <b>{json["hourly"]["temperature_2m"][9]}</b>{json["hourly_units"]["temperature_2m"]}\n'
+        f'    Ветер:                <b>{json["hourly"]["windspeed_10m"][9]}</b> {json["hourly_units"]["windspeed_10m"]}'
+        f'\n    Направление: {wind_direction_to_word(json["hourly"]["winddirection_10m"][9])}\n'
+
+        f'12:00\n'
+        f'    Температура: <b>{json["hourly"]["temperature_2m"][12]}</b>{json["hourly_units"]["temperature_2m"]}\n'
+        f'    Ветер:                <b>{json["hourly"]["windspeed_10m"][12]}</b> {json["hourly_units"]["windspeed_10m"]}'
+        f'\n    Направление: {wind_direction_to_word(json["hourly"]["winddirection_10m"][12])}\n'
+
+        f'15:00\n'
+        f'    Температура: <b>{json["hourly"]["temperature_2m"][15]}</b>{json["hourly_units"]["temperature_2m"]}\n'
+        f'    Ветер:                <b>{json["hourly"]["windspeed_10m"][15]}</b> {json["hourly_units"]["windspeed_10m"]}'
+        f'\n    Направление: {wind_direction_to_word(json["hourly"]["winddirection_10m"][15])}\n'
+
+        f'18:00\n'
+        f'    Температура: <b>{json["hourly"]["temperature_2m"][18]}</b>{json["hourly_units"]["temperature_2m"]}\n'
+        f'    Ветер:                <b>{json["hourly"]["windspeed_10m"][18]}</b> {json["hourly_units"]["windspeed_10m"]}'
+        f'\n    Направление: {wind_direction_to_word(json["hourly"]["winddirection_10m"][18])}\n'
+
+        f'21:00\n'
+        f'    Температура: <b>{json["hourly"]["temperature_2m"][21]}</b>{json["hourly_units"]["temperature_2m"]}\n'
+        f'    Ветер:                <b>{json["hourly"]["windspeed_10m"][21]}</b> {json["hourly_units"]["windspeed_10m"]}'
+        f'\n    Направление: {wind_direction_to_word(json["hourly"]["winddirection_10m"][21])}\n'
+    )
+    tomorrow_response_text = f' '
+    week_response_text = f' '
+    week2_response_text = f' '
+
+
 
 # @logger.catch
 def request_weather():
-    time.sleep(5)
+    time.sleep(2)
     try:
         res = requests.get(
             'http://api.open-meteo.com/v1/forecast?latitude=45.04&longitude=38.98&hourly=temperature_2m,'
@@ -27,14 +104,24 @@ def request_weather():
         last_update_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
         logger.info(f"Weather = {js_wt_req}")
-        text_response.weather_response_text(js_wt_req, last_update_time)
+        weather_response_text(js_wt_req, last_update_time)
+        logger.info(f'{current_file} code:{res.status_code} ')
     except Exception as e:
-        logger.error(f'{current_file} <{inspect.currentframe().f_code.co_name}> {e}')
+        logger.error(f'{e}')
 
-    logger.info(f'{current_file} code:{res.status_code} ')
-    logger.info(f'Next weather request in an {timeout/60} minutes')
+    logger.info(f'Next weather request in an {timeout / 60} minutes')
     time.sleep(timeout)
 
 
 t = threading.Thread(target=request_weather)
 t.start()
+
+
+def weather_get(timers):
+    get_weather = {
+        'today': today_response_text,
+        'tomorrow': tomorrow_response_text,
+        'week': week_response_text,
+        'week2': week2_response_text
+    }
+    return get_weather[timers]
